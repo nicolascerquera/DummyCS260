0d91d0a7ca4df3e31993107dd5c1357aba79c36a
apple313 <yangseung0105@gmail.com>
2018-01-22 17:56:51 -0700

    Seung-Hee added another comment.

--- NewTestSoftware/NewTestSoftware.UI/Properties/AssemblyInfo.cs
+++ NewTestSoftware/NewTestSoftware.UI/Properties/AssemblyInfo.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //Seung-Hee added this line. 
+//Seung-Hee added another line. 
 [assembly: AssemblyTitle("NewTestSoftware.UI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
